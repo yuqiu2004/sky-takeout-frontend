b546aad41d325404fda0ae2ce6bfab3d32d0afa8
yuqiu2004 <2506852507@qq.com>
2024-10-12 20:04:29 +0800

    fix : change name and format router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Layout from "@/layout/index.vue";
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+import Vue from 'vue';
+import Router from 'vue-router';
+import Layout from '@/layout/index.vue';
 import {
   getToken,
   setToken,
@@ -11,8 +13,8 @@ import {
   setUserInfo,
   getUserInfo,
   removeUserInfo
-} from "@/utils/cookies";
-import store from "@/store";
+} from '@/utils/cookies';
+import store from '@/store';
 
 Vue.use(Router);
 
@@ -26,121 +28,118 @@ const router = new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/login",
+      path: '/login',
       component: () =>
-        import(/* webpackChunkName: "login" */ "@/views/login/index.vue"),
-      meta: { title: "苍穹外卖", hidden: true, notNeedAuth: true }
+        import(/* webpackChunkName: "login" */ '@/views/login/index.vue'),
+      meta: { title: '后台管理登录', hidden: true, notNeedAuth: true }
     },
     {
-      path: "/404",
-      component: () => import(/* webpackChunkName: "404" */ "@/views/404.vue"),
-      meta: { title: "苍穹外卖", hidden: true, notNeedAuth: true }
+      path: '/404',
+      component: () => import(/* webpackChunkName: "404" */ '@/views/404.vue'),
+      meta: { title: '出错了', hidden: true, notNeedAuth: true }
     },
     {
-      path: "/",
+      path: '/',
       component: Layout,
-      redirect: "/dashboard",
+      redirect: '/dashboard',
       children: [
         {
-          path: "dashboard",
+          path: 'dashboard',
           component: () =>
-            import(/* webpackChunkName: "dashboard" */ "@/views/dashboard/index.vue"),
-          name: "Dashboard",
+            import(/* webpackChunkName: "dashboard" */ '@/views/dashboard/index.vue'),
+          name: 'Dashboard',
           meta: {
-            title: "工作台",
-            icon: "dashboard",
+            title: '工作台',
+            icon: 'dashboard',
             affix: true
           }
         },
-		{
-          path: "/statistics",
+		    {
+          path: '/statistics',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/statistics/index.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/statistics/index.vue'),
           meta: {
-            title: "数据统计",
-            icon: "icon-statistics"
+            title: '数据统计',
+            icon: 'icon-statistics'
           }
         },
         {
-          path: "order",
+          path: 'order',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/orderDetails/index.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/orderDetails/index.vue'),
           meta: {
-            title: "订单管理",
-            icon: "icon-order"
+            title: '订单管理',
+            icon: 'icon-order'
           }
         },
         {
-          path: "setMeal",
+          path: 'setMeal',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/setMeal/index.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/setMeal/index.vue'),
           meta: {
-            title: "套餐管理",
-            icon: "icon-combo"
+            title: '套餐管理',
+            icon: 'icon-combo'
           }
         },
         {
-          path: "dish",
+          path: 'dish',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/dish/index.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/dish/index.vue'),
           meta: {
-            title: "菜品管理",
-            icon: "icon-dish"
+            title: '菜品管理',
+            icon: 'icon-dish'
           }
         },
         {
-          path: "/dish/add",
+          path: '/dish/add',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/dish/addDishtype.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/dish/addDishtype.vue'),
           meta: {
-            title: "添加菜品",
+            title: '添加菜品',
             hidden: true
           }
         },
-        
         {
-          path: "category",
+          path: 'category',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/category/index.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/category/index.vue'),
           meta: {
-            title: "分类管理",
-            icon: "icon-category"
+            title: '分类管理',
+            icon: 'icon-category'
           }
         },
         {
-          path: "employee",
+          path: 'employee',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/employee/index.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/employee/index.vue'),
           meta: {
-            title: "员工管理",
-            icon: "icon-employee"
+            title: '员工管理',
+            icon: 'icon-employee'
           }
         },
-        
         {
-          path: "/employee/add",
+          path: '/employee/add',
           component: () =>
-            import(/* webpackChunkName: "dashboard" */ "@/views/employee/addEmployee.vue"),
+            import(/* webpackChunkName: "dashboard" */ '@/views/employee/addEmployee.vue'),
           meta: {
-            title: "添加员工",
+            title: '添加员工',
             hidden: true
           }
         },
-        
         {
-          path: "/setMeal/add",
+          path: '/setMeal/add',
           component: () =>
-            import(/* webpackChunkName: "shopTable" */ "@/views/setMeal/addSetMeal.vue"),
+            import(/* webpackChunkName: "shopTable" */ '@/views/setMeal/addSetMeal.vue'),
           meta: {
-            title: "添加套餐",
+            title: '添加套餐',
             hidden: true
           }
         }
       ]
     },
     {
-      path: "*",
-      redirect: "/404",
+      path: '*',
+      redirect: '/404',
       meta: { hidden: true }
     }
   ]
